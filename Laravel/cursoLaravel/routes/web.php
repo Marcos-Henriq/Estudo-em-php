65cc73755e60fdcb79f343b8d4ec61d7ab2f22a0
Marcos-Henriq <mh7505570@gmail.com>
2022-07-24 22:58:00 -0300

    Rotas e Views com Laravel

--- Laravel/cursoLaravel/routes/web.php
+++ Laravel/cursoLaravel/routes/web.php
@@ -7,12 +7,24 @@ use Illuminate\Support\Facades\Route;
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+AQUI QUE SE CRIA ROTAS DE ACESSO AS PAGINAS.
 |
 */
 
+
+// INDEX
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+// OUTRAS PAGIINAS
+
+Route::get('/contact', function () { //POR ONDE O USUARIO ACESSA
+    return view('contact'); // E OQ RETORNA = contact.blade.php
+});
+
+Route::get('/produtos', function () {
+    return view('product');
 });
